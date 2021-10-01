7b53513b41ffbdeb19d3181936cfe88528180006
Kelly cantillo <kellyj.cantillo@gmail.com>
2021-10-01 16:27:44 -0700

    abc

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Print alfabet
+ *
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for(ch= 'a', ch=<'z',ch++)
+		puschar(ch);
+
+	puschar('\n');
+
+	return (0);
+}
+
+
