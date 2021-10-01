8eca685584e17b7005eddcb4c883405c40d70ea1
Kelly cantillo <kellyj.cantillo@gmail.com>
2021-10-01 16:47:39 -0700

    AaZz

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,11 +12,17 @@ int main(void)
 {
 	char ch;
 
-	for (ch = 'A';  ch <= 'Z'; ch++)
+	for (ch = 'a';  ch <= 'z'; ch++)
 		putchar(ch);
 
 	putchar('\n');
 
+	
+	for (ch = 'A';  ch <= 'Z'; ch++)
+		putchar(ch);
+	
+	putchar('\n');
+	
 	return (0);
 }
 
