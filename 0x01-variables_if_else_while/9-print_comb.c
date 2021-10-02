5bc9bf442ce037fa575fa534ef97f660449c97e5
Kelly cantillo <kellyj.cantillo@gmail.com>
2021-10-01 18:00:30 -0700

    elfe if while

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Print number
+ *
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = '0';  n <= '9'; n++)
+	{
+		putchar(n);
+		if (n != 9)
+			putchar(',');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
+
