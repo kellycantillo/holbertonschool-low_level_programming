bbf62ee93354ae7cdab1af860e9367a9ec65dcf8
kellycantillo <80362584+kellycantillo@users.noreply.github.com>
2021-10-01 22:07:22 -0500

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,7 @@
  *   * main - Prints tamaño
  *     *
  *       * Return: Always 0 (Success)
- *  
+ *
  */
 int main(void)
 {
@@ -13,7 +13,7 @@ int main(void)
 	long int c;
 	long long int d;
 	float e;
-	
+
 	printf("Size of a char: %d byte(s)\n", sizeof(a));
 	printf("Size of an int: %d byte(s)\n", sizeof(b));
 	printf("Size of a long int: %d byte(s)\n", sizeof(c));
