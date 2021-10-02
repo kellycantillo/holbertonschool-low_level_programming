d5e70e9af66c58b90a839b331a388881bd87865d
kellycantillo <80362584+kellycantillo@users.noreply.github.com>
2021-10-01 19:30:54 -0500

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,5 +1,3 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
 /**
@@ -13,7 +11,7 @@ int main(void)
 	int n;
 
 	for (n = 0;  n <= 9; n++)
-		printf("%d",n);
+		putchar(n);
 
 	putchar('\n');
 
