c3ca984d64fe36f57015131c04e6b123afe02982
Kelly cantillo <kellyj.cantillo@gmail.com>
2021-10-01 17:24:44 -0700

    variables is else

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Print number
+ *
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0;  n <= 9; n++)
+		putchar(n);
+
+	putchar('\n');
+
+	return (0);
+}
+
+
