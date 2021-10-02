d672ccff190e90ad0e31ea92aff60dacd60be014
Kelly cantillo <kellyj.cantillo@gmail.com>
2021-10-01 19:21:16 -0700

    message

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,15 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
- *   * main - Prints a message
- *     *
- *       * Return: Always 0 (Success)
- *         */
+ * main - Prints a message
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }
+
