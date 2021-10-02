720156d87e7ff1372bd7ecc1b8d1a553d2d95b21
Kelly cantillo <kellyj.cantillo@gmail.com>
2021-10-01 17:34:26 -0700

    variables is else

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Print alfabet
+ *
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z';  ch <= 'a'; ch++)
+		putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
+
+
